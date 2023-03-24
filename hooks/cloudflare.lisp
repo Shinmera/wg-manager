3b4ee00e7c33b264592698d32021c00c0f37ae13
Shinmera <shinmera@tymoon.eu>
2023-03-24 15:52:16 +0100

    cloudflare fix

--- hooks/cloudflare.lisp
+++ hooks/cloudflare.lisp
@@ -2,7 +2,7 @@
 exec sbcl \
   --noinform \
   --disable-debugger \
-  --eval "(ql:quickload '(drakma com.inuoe.jzon) :silent T)" \
+  --eval "(ql:quickload '(drakma alexandria com.inuoe.jzon) :silent T)" \
   --load "$0" \
   --eval "(wg-manager.cloudflare::main)" \
   --quit \
@@ -24,7 +24,7 @@ exec sbcl \
                 (format NIL "https://api.cloudflare.com/client/v4/~a" endpoint)
                 :method method
                 :content-type "application/json"
-                :content (com.inuoe.jzon:stringify payload)
+                :content (com.inuoe.jzon:stringify (alexandria:alist-hash-table payload))
                 :additional-headers `(("Authorization" . ,(format NIL "Bearer ~a" *bearer*)))
                 :want-stream T))))
     (if (gethash "success" data)
@@ -32,9 +32,17 @@ exec sbcl \
         (error "API call failed!~%~%~a" (com.inuoe.jzon:stringify data :pretty T)))))
 
 (defun find-record (name)
-  (loop for record across (gethash "result" (call :get (format NIL "zones/~a/dns_records" *zone*)))
+  (loop for record across (call :get (format NIL "zones/~a/dns_records" *zone*))
         do (when (string-equal name (gethash "name" record))
              (return (gethash "id" record)))))
+
+(defun sanitize (name)
+  (with-output-to-string (out)
+    (loop for c across name
+          do (cond ((or (alphanumericp c) (char= #\- c))
+                    (write-char c out))
+                   ((char= #\/ c)
+                    (write-char #\- out))))))
 
 (defun main ()
   (destructuring-bind (self change ipv4 name &optional public-key note) sb-ext:*posix-argv*
@@ -42,22 +50,21 @@ exec sbcl \
     (setf *zone* (uiop:getenv "WG_CLOUDFLARE_ZONE"))
     (setf *bearer* (uiop:getenv "WG_CLOUDFLARE_BEARER"))
     (setf *suffix* (uiop:getenv "WG_CLOUDFLARE_SUFFIX"))
-    (let ((name (format NIL "~a~@[.~a~]" name *suffix*)))
+    (let* ((name (format NIL "~a~@[.~a~]" (sanitize name) *suffix*))
+           (data `(("content" . ,ipv4)
+                   ("name" . ,name)
+                   ("proxied" . NIL)
+                   ("type" . "A")
+                   ("comment" . ,(or note 'null))
+                   ("ttl" . 1))))
       (cond ((string-equal change "add")
-             (call :post (format NIL "zones/~a/dns_records" *zone*)
-                   `(("content" . ,ipv4)
-                     ("name" . ,name)
-                     ("proxied" . NIL)
-                     ("type" . "A")
-                     ("comment" . ,(or note 'null))
-                     ("ttl" . 1))))
+             (call :post (format NIL "zones/~a/dns_records" *zone*) data))
             ((string-equal change "remove")
-             (call :delete (format NIL "zones/~a/dns_records/~a" *zone* (find-record name))))
+             (let ((record (find-record name)))
+               (when record
+                 (call :delete (format NIL "zones/~a/dns_records/~a" *zone* record)))))
             ((string-equal change "edit")
-             (call :put (format NIL "zones/~a/dns_records/~a" *zone* (find-record name))
-                   `(("content" . ,ipv4)
-                     ("name" . ,name)
-                     ("proxied" . NIL)
-                     ("type" . "A")
-                     ("comment" . ,(or note 'null))
-                     ("ttl" . 1))))))))
+             (let ((record (find-record name)))
+               (if record
+                   (call :put (format NIL "zones/~a/dns_records/~a" *zone* record) data)
+                   (call :post (format NIL "zones/~a/dns_records" *zone*) data))))))))
